8013ccc7c7fdf74a1efc906f848b2aebf4b8f490
Mateus Barbosa <72516703+mateuscbarbosa@users.noreply.github.com>
2022-07-06 17:05:08 -0300

    Testes de update na UserService (com erros aleatórios)

--- src/test/java/br/com/petshoptchutchucao/agenda/service/UserServiceTest.java
+++ src/test/java/br/com/petshoptchutchucao/agenda/service/UserServiceTest.java
@@ -19,6 +19,7 @@ import com.mongodb.MongoException;
 
 import br.com.petshoptchutchucao.agenda.dto.UserFormDto;
 import br.com.petshoptchutchucao.agenda.dto.UserOutputDto;
+import br.com.petshoptchutchucao.agenda.dto.UserUpdateFormDto;
 import br.com.petshoptchutchucao.agenda.model.Profile;
 import br.com.petshoptchutchucao.agenda.model.Status;
 import br.com.petshoptchutchucao.agenda.model.User;
@@ -36,7 +37,7 @@ public class UserServiceTest {
 
 	@Mock
 	private ProfileRepository profileRepository;
-	
+
 	@Mock
 	private UserRepository userRepository;
 
@@ -44,41 +45,88 @@ public class UserServiceTest {
 	private UserService service;
 
 	@Test
-	void couldNotRegisterAUserWithInsuficientData() {
-		profilesVetor[0]=0;
-		
-		UserFormDto userForm = new UserFormDto("","",profilesVetor);
+	void couldNotRegisterAnUserWithInsuficientData() {
+		profilesVetor[0] = 0;
+
+		UserFormDto userForm = new UserFormDto("", "", profilesVetor);
 
 		User user = new User(userForm.getEmail(), userForm.getName(), profilesList, Status.ATIVO);
 
 		Mockito.when(modelMapper.map(userForm, User.class)).thenReturn(user);
 
 		Mockito.when(profileRepository.getById(profilesVetor[0])).thenThrow(MongoException.class);
-		
-		assertThrows(MongoException.class, () -> service.register(userForm));
 
+		assertThrows(MongoException.class, () -> service.register(userForm));
 	}
-	
+
 	@Test
-	void couldRegisterAUserWithCompleteData() {
-		profilesVetor[0]=0;
+	void couldRegisterAnUserWithCompleteData() {
+		profilesVetor[0] = 0;
 		Profile profile = new Profile();
 		UserFormDto userForm = new UserFormDto("teste@teste.com.br", "Teste", profilesVetor);
+
+		Mockito.when(profileRepository.getById(profilesVetor[0])).thenReturn(Optional.of(profile));
+
+		User user = new User(userForm.getEmail(), userForm.getName(), profilesList, Status.ATIVO);
+
+		Mockito.when(modelMapper.map(userForm, User.class)).thenReturn(user);
+
+		Mockito.when(modelMapper.map(user, UserOutputDto.class))
+				.thenReturn(new UserOutputDto(null, user.getEmail(), user.getName()));
+
+		UserOutputDto userOutput = service.register(userForm);
+
+		Mockito.verify(userRepository).save(Mockito.any());
+
+		assertEquals(userForm.getEmail(), userOutput.getEmail());
+		assertEquals(userForm.getName(), userOutput.getName());
+
+	}
+
+	@Test
+	void couldNotUpdateAnInexisteUserId() {
+		profilesVetor[0] = 10;
+		UserUpdateFormDto userUpdate = new UserUpdateFormDto("123456",
+															"teste@email.com.br",
+															"Teste",
+															profilesVetor,
+															Status.ATIVO,
+															"$3nh@1");
+
+		Mockito.when(userRepository.findById(userUpdate.getId())).thenThrow(MongoException.class);
+
+		assertThrows(MongoException.class, () -> service.update(userUpdate));
+	}
+
+	@Test
+	void couldUpdateAnExistentUserId() {
+		profilesVetor[0] = 10;
+		
+		Profile profile = new Profile();
+
+		UserUpdateFormDto userUpdate = new UserUpdateFormDto("123456",
+															"teste@email.com.br",
+															"Teste",
+															profilesVetor,
+															Status.ATIVO,
+															"$3nh@1");
 		
 		Mockito.when(profileRepository.getById(profilesVetor[0])).thenReturn(Optional.of(profile));
 		
-		User user = new User(userForm.getEmail(),userForm.getName(),profilesList, Status.ATIVO);
-		
-		Mockito.when(modelMapper.map(userForm, User.class)).thenReturn(user);
+		User user = modelMapper.map(userUpdate, User.class);
+
+		Mockito.when(modelMapper.map(userUpdate, User.class)).thenReturn(user);
+
+		Mockito.when(userRepository.findById(userUpdate.getId()).get()).thenReturn(user);
 		
 		Mockito.when(modelMapper.map(user, UserOutputDto.class)).thenReturn(new UserOutputDto(null, user.getEmail(), user.getName()));
-		
-		UserOutputDto userOutput = service.register(userForm);
-		
+
+		UserOutputDto userOutput = service.update(userUpdate);
+
 		Mockito.verify(userRepository).save(Mockito.any());
-		
-		assertEquals(userForm.getEmail(), userOutput.getEmail());
-		assertEquals(userForm.getName(), userOutput.getName());
-		
+
+		assertEquals(userUpdate.getEmail(), userOutput.getEmail());
+		assertEquals(userUpdate.getName(), userOutput.getName());
 	}
+
 }
