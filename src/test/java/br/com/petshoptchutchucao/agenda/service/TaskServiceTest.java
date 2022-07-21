07902c899dc9cbbe743b8f3ca637cda281e89fb5
Mateus Barbosa <72516703+mateuscbarbosa@users.noreply.github.com>
2022-07-21 15:00:13 -0300

    Testes de atualização de serviços adicionados a TaskServiceTest

--- src/test/java/br/com/petshoptchutchucao/agenda/service/TaskServiceTest.java
+++ src/test/java/br/com/petshoptchutchucao/agenda/service/TaskServiceTest.java
@@ -1,8 +1,10 @@
 package br.com.petshoptchutchucao.agenda.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +16,8 @@ import org.modelmapper.ModelMapper;
 
 import br.com.petshoptchutchucao.agenda.dto.TaskFormDto;
 import br.com.petshoptchutchucao.agenda.dto.TaskOutputDto;
+import br.com.petshoptchutchucao.agenda.dto.TaskUpdateFormDto;
+import br.com.petshoptchutchucao.agenda.infra.BusinessRulesException;
 import br.com.petshoptchutchucao.agenda.model.Size;
 import br.com.petshoptchutchucao.agenda.model.Spicies;
 import br.com.petshoptchutchucao.agenda.model.Task;
@@ -50,6 +54,38 @@ class TaskServiceTest {
 		
 		assertEquals(taskForm.getName(), taskDto.getName());
 		assertEquals(taskForm.getPrice(), taskDto.getPrice());
+	}
+	
+	@Test
+	void couldNotUpdateATaskWithIncorrectId() {
+		TaskUpdateFormDto taskUpdate = new TaskUpdateFormDto("123456", "Serviço Teste", Spicies.CACHORRO, Size.MÉDIO, new BigDecimal(100));
+		
+		Mockito.when(taskRepository.findById(taskUpdate.getId())).thenThrow(BusinessRulesException.class);
+		
+		assertThrows(BusinessRulesException.class,() -> service.update(taskUpdate));
+	}
+	
+	@Test
+	void couldUpdateATaskWithCorrectId() {
+		TaskUpdateFormDto taskUpdate = new TaskUpdateFormDto("123456", "Serviço Teste", Spicies.CACHORRO, Size.MÉDIO, new BigDecimal(100));
+		
+		Task task = new Task(taskUpdate.getId(),
+							taskUpdate.getName(),
+							taskUpdate.getSpicies(),
+							taskUpdate.getSize(),
+							taskUpdate.getPrice());
+		
+		Mockito.when(taskRepository.findById(taskUpdate.getId())).thenReturn(Optional.of(task));
+		
+		Mockito.when(modelMapper.map(task, TaskOutputDto.class)).thenReturn(new TaskOutputDto(task.getId(), task.getName(), task.getPrice()));
+		
+		TaskOutputDto taskDto = service.update(taskUpdate);
+		
+		Mockito.verify(taskRepository).save(Mockito.any());
+		
+		assertEquals(taskUpdate.getId(), taskDto.getId());
+		assertEquals(taskUpdate.getName(), taskDto.getName());
+		assertEquals(task.getPrice(), taskDto.getPrice());
 	}
 
 }
